797ece86b0f6589ea22f1002026137f493b6702e
Dean Hiller <dean@alvazan.com>
2013-11-22 11:59:16 -0700

    fix this stuff

--- src/main/java/org/apache/log4j/helpers/QuietWriter.java
+++ src/main/java/org/apache/log4j/helpers/QuietWriter.java
@@ -1,12 +1,31 @@
 package org.apache.log4j.helpers;
 
 import java.io.FilterWriter;
+import java.io.IOException;
 import java.io.Writer;
 
 public class QuietWriter extends FilterWriter {
 
 	protected QuietWriter(Writer arg0) {
 		super(arg0);
+	}
+
+	@Override
+	public void flush() {
+		try {
+			super.flush();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void write(String str) {
+		try {
+			super.write(str);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
---
+++ src/main/java/org/apache/log4j/spi/LoggingEvent.java
@@ -0,0 +1,10 @@
+package org.apache.log4j.spi;
+
+import org.apache.log4j.Level;
+
+public class LoggingEvent {
+
+	public org.apache.log4j.Level getLevel() {
+		return Level.INFO;
+	}
+}
